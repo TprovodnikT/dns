2434f4ea1bb4f8cb3d265f84ee231ffb1ae719b9
tprovodnikt <tprovodnikt@gmail.com>
2016-04-22 17:42:03 +0300

    remove tmp vim files

--- create_domains_hash.pl
+++ create_domains_hash.pl
@@ -1,4 +1,56 @@
 #!/usr/bin/perl
 use util::subroutine;
+$pathToFile = $ARGV[0];
+$pathToOut = $ARGV[1];
+if (length($ARGV[0]) == 0) {
+  $pathToFile = '/home/jn/workspace/bash_tut/perl_tutorial/active_domains';
+}
+if (length($ARGV[1]) == 0) {
+  $pathToOut = '/home/jn/workspace/bash_tut/perl_tutorial/domains_full_info';
+}
 
-sub 
+open ($fhOut, '>', $pathToOut);
+#print "\nHere: $pathToFile\n";
+open($fh, '<:encoding(UTF-8)', $pathToFile)
+  or die "Could not open file $pathtoFile";
+
+while ($row = <$fh>) {
+  chomp $row;
+  print $fhOut "domain:\n" . $row;
+  @domainRecords = `host $row`;
+  @domainIps = ();
+  @mxRecords = ();
+  for $record (@domainRecords){
+    if ($record =~ m/has\ address\ (.*)$/){
+      $domainIp = $1;
+      push @domainIps, $domainIp; 
+    }elsif (index($record, "mail is handled") > -1){
+      $record =~ /.*\ (.*)\.$/;
+      $mxRec = $1;
+      push @mxRecords, $mxRec;
+    }
+  }
+  
+  print $fhOut "\ndomain_ips:\n";
+  for $domainIp (@domainIps) {
+    print $fhOut $domainIp . "\n";
+  }
+
+  print $fhOut "mx_records:\n";
+  for $mxRecord (@mxRecords){
+    if (length($mxRecord) > 0) {
+      @mxRecIps = `host $mxRecord`;
+      print $fhOut "$mxRecord\nmx_ips:\n";
+      foreach $mxRecIp (@mxRecIps) {
+        $mxRecIp =~ m/has\ address\ (.*)$/;
+	$mxRecIp = $1;
+        print $fhOut $mxRecIp . "\n";
+      } 
+    }
+  }
+
+  print $fhOut "\n";
+  
+}
+print "@domains\n";
+#sub 
